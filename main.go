0dd4662ea695d2b8438d224b62f036d6e45a5d01
nellfs <heron.x.dev@gmail.com>
2023-10-23 18:41:57 -0300

    basic grid map

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type Game struct{}
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+	TileSize     = 32
+)
+
+type Game struct {
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -15,15 +24,25 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, ":)")
+	for i := int32(0); i < ScreenWidth/TileSize; i++ {
+		for j := int32(0); j < ScreenWidth/TileSize; j++ {
+			if (i+j)%2 == 0 {
+				vector.DrawFilledRect(screen, float32(i*TileSize), float32(j*TileSize), TileSize, TileSize, color.RGBA{0, 0, 255, 255}, false)
+			} else {
+				vector.DrawFilledRect(screen, float32(i*TileSize), float32(j*TileSize), TileSize, TileSize, color.RGBA{0, 0, 100, 255}, false)
+			}
+		}
+	}
+	vector.DrawFilledCircle(screen, 250, 250, 50, color.White, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return ScreenWidth, ScreenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("nellfs game")
+	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
+	ebiten.SetWindowTitle("Snake Tetris")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
